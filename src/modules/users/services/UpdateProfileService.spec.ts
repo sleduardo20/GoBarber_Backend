9f287f6f0cc8221c3337e2b88dc55fc95e9e4316
slsleduardo20 <sl.eduaro20@gmail.com>
2020-05-14 01:47:14 -0300

    implementaçao UpdateProfileService

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -1,4 +1,4 @@
-// import AppError from '@shared/erros/AppError';
+import AppError from '@shared/erros/AppError';
 
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
@@ -34,5 +34,80 @@ describe('UpdateProfile', () => {
 
     expect(updatedUser.name).toBe('Eduardo2');
     expect(updatedUser.email).toBe('eduardo2@gmail.com');
+  });
+
+  it('should not be able to change another user email', async () => {
+    await fakeUsersRepository.create({
+      name: 'Eduardo2',
+      email: 'eduardo@gmail.com',
+      password: '123456',
+    });
+
+    const user = await fakeUsersRepository.create({
+      name: 'Test',
+      email: 'eduardo@gmail.com',
+      password: '123456',
+    });
+
+    await expect(
+      updateProfile.execute({
+        user_id: user.id,
+        name: 'Eduardo2',
+        email: 'eduardo@gmail.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able update the password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Eduardo',
+      email: 'eduardo@gmail.com',
+      password: '123456',
+    });
+
+    const updatedUser = await updateProfile.execute({
+      user_id: user.id,
+      name: 'Eduardo2',
+      email: 'eduardo2@gmail.com',
+      old_password: '123456',
+      password: '123123',
+    });
+
+    expect(updatedUser.password).toBe('123123');
+  });
+
+  it('should not be able update the password without old password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Eduardo',
+      email: 'eduardo@gmail.com',
+      password: '123456',
+    });
+
+    await expect(
+      updateProfile.execute({
+        user_id: user.id,
+        name: 'Eduardo2',
+        email: 'eduardo2@gmail.com',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able update the password with wrong old password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Eduardo',
+      email: 'eduardo@gmail.com',
+      password: '123456',
+    });
+
+    await expect(
+      updateProfile.execute({
+        user_id: user.id,
+        name: 'Eduardo2',
+        email: 'eduardo2@gmail.com',
+        old_password: 'wrong-old-password',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
--- src/modules/users/services/UpdateProfileService.ts
+++ src/modules/users/services/UpdateProfileService.ts
@@ -24,14 +24,50 @@ class UpdateProfile {
     private hashProvider: IHashProvider,
   ) {}
 
-  public async execute({ user_id, name, email }: IRequest): Promise<User> {
+  public async execute({
+    user_id,
+    name,
+    email,
+    password,
+    old_password,
+  }: IRequest): Promise<User> {
     const user = await this.usersRepository.findById(user_id);
 
     if (!user) {
       throw new AppError('User not found');
     }
 
-    return user;
+    const userWithUpdatedEmail = await this.usersRepository.findByEmail(email);
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user_id) {
+      throw new AppError('E-mail already in use');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    if (password && !old_password) {
+      throw new AppError(
+        'You need tor inform old password to set a new password.',
+      );
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await this.hashProvider.compareHash(
+        old_password,
+        user.password,
+      );
+
+      if (!checkOldPassword) {
+        throw new AppError('Old password does not math.');
+      }
+    }
+
+    if (password) {
+      user.password = await this.hashProvider.genereteHash(password);
+    }
+
+    return this.usersRepository.salve(user);
   }
 }
 
