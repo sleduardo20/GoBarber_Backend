bb7c6d210a959cca8be0cc0e958c489731a90185
slsleduardo20 <sl.eduaro20@gmail.com>
2020-05-18 10:52:55 -0300

    validação das rotas

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAutheticated from '@modules/users/infra/http/middlewares/ensureAutheticated';
 import AppointmentController from '../controllers/AppointmentsController';
@@ -9,7 +10,17 @@ const appointmentsController = new AppointmentController();
 const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAutheticated);
-appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date,
+    },
+  }),
+  appointmentsController.create,
+);
+
 appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAutheticated from '@modules/users/infra/http/middlewares/ensureAutheticated';
 import ProvidersController from '../controllers/ProvidersController';
@@ -13,12 +14,24 @@ const providerDayAvailabilityController = new ProviderDayAvailabilityController(
 providersRouter.use(ensureAutheticated);
 
 providersRouter.get('/', providersController.index);
+
 providersRouter.get(
   '/:provider_id/month-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
   providerMonthAvailabilityController.index,
 );
+
 providersRouter.get(
   '/:provider_id/day-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
   providerDayAvailabilityController.index,
 );
 
